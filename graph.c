08a86af3982e51a8292c8717e0c7409e9f4678c3
Hrishabh Pandey <44145550+hrs2203@users.noreply.github.com>
2019-09-18 16:56:47 +0530

    Add files via upload
    removing deletion and editing addition process

--- graph.c
+++ graph.c
@@ -1,173 +1,163 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct Neighbour{
+typedef struct Neighbour
+{
     int val;
-    struct Neighbour* next;
-}neighbour;
+    struct Neighbour *next;
+    struct Neighbour *prev;
+} neighbour;
 
-typedef struct Node{
+typedef struct Node
+{
     int val;
-    struct Node* next;
-    struct Neighbour* link;
-}node;
+    struct Node *next;
+    struct Node *prev;
+    struct Neighbour *link;
+} node;
 
-node* headNode;
+node *headNode;
 
-node* findNode(node* head,int key){
-    node* temp = (node*)malloc(sizeof(node));
-    return temp;
-}
-
-node* newNode(int key){
-    node* temp;
-    temp = (node*)malloc(sizeof(node));
+node *newNode(int key)
+{
+    node *temp;
+    temp = (node *)malloc(sizeof(node));
     temp->val = key;
     temp->next = NULL;
+    temp->prev = NULL;
     temp->link = NULL;
     return temp;
 }
 
-neighbour* newNeighbour(int key){
-    neighbour* temp;
-    temp = (neighbour*)malloc(sizeof(neighbour));
+neighbour *newNeighbour(int key)
+{
+    neighbour *temp;
+    temp = (neighbour *)malloc(sizeof(neighbour));
     temp->next = NULL;
+    temp->prev = NULL;
     temp->val = key;
     return temp;
 }
 
-node* insertNode(node* temp_a,int key){
-    // temp_a = insertNode(temp_a,key)
-    if (temp_a == NULL){
-        return newNode(key);
-    }
-    node* temp = temp_a;
-    while(temp->next){
-        temp = temp->next;
-    }temp->next = newNode(key);
-    return temp_a;
-}
-
-void show_node(node* head_a){
-    node* head = head_a;
+void show_node(node *head_a)
+{
+    node *head = head_a;
     printf("all nodes are -> ");
-    while (head){
-        printf("%d ",head->val);
+    while (head)
+    {
+        printf("%d ", head->val);
         head = head->next;
-    }printf("\n");
+    }
+    printf("\n");
 }
 
-void show_neighbour(node* temp){
-    neighbour* point = temp->link;
-    while(point){
-        printf("%d ",point->val);
+void show_neighbour(node *temp)
+{
+    neighbour *point = temp->link;
+    while (point)
+    {
+        printf("%d ", point->val);
         point = point->next;
-    }printf("\n");
+    }
+    printf("\n");
 }
 
-
-void show_all(node* head_a){
-    node* head = head_a;
-    while (head){
-        printf("node is %d and its neighbours are -> ",head->val);
+void show_all(node *head_a)
+{
+    node *head = head_a;
+    while (head)
+    {
+        printf("node is %d and its neighbours are -> ", head->val);
         show_neighbour(head);
         head = head->next;
-    }printf("\n");
+    }
+    printf("\n");
 }
 
-node* insertNeighbour(node* temp_a,int key_node,int key){
-    // insert neighbour in temp node
-    // headNode = insertNeighbour(headNode,key_node,key) -> to use it
+node* getNode(node* temp,int key){
+    // search node with value=key
+    while(temp){
+        if (temp->val == key){
+            return temp;
+        }temp=temp->next;
+    }
+    return temp;
+}
 
+void show_rev(node *head_a)
+{
+    node *head = getNode(head_a, 1);
+    printf("all nodes are -> ");
+    while (head)
+    {
+        printf("%d ", head->val);
+        head = head->prev;
+    }
+    printf("\n");
+}
+
+node *insertNode(node *temp_a, int key){
+    node* home = temp_a;
+    node *temp = newNode(key);
+    temp->next = home;
+    if (home){
+        home->prev = temp;
+    }
+    return temp;
+}
+
+neighbour* getNeighbour(node* temp_a,int key_node,int key_neighbour){
+    // search neighbour with value=key_neighbour
     node* head = temp_a;
-    node* temp = temp_a;
-    while(temp!=NULL){
-        if (temp->val == key_node){
-            break;
-        }temp = temp->next;
+    node* parentNode = getNode(head,key_node);
+    neighbour* neighbourNode = parentNode->link;
+    while (neighbourNode){
+        if (neighbourNode->val = key_neighbour){
+            return neighbourNode;
+        }neighbourNode = neighbourNode->next;
+    }return neighbourNode;
+}
+
+node *insertNeighbour(node *temp_a, int key_node, int key){
+    node *head = temp_a;
+    node* temp = getNode(head,key_node);
+    if (temp){
+        neighbour *insertLink = temp->link;
+        temp->link = newNeighbour(key);
+        temp->link->next = insertLink;
     }
-    if (temp!=NULL){
-        // printf("node found %d\n",temp->val);
-        neighbour* insertLink = temp->link;
-        if (insertLink == NULL){
-            temp->link = newNeighbour(key);
-        }else{
-            while (insertLink->next!=NULL){
-                insertLink = insertLink->next;
-            }
-            insertLink->next = newNeighbour(key);
-        }
-    }else{
+    else
         printf("node not found\n");
-    }
 
     return head;
 }
 
-node* deleteNeighbour(node* temp_a,int key_node,int key){
-    node* head = temp_a;
-    node* temp = temp_a;
-    while(temp!=NULL){
-        if (temp->val == key_node){
-            break;
-        }temp = temp->next;
-    }
-    if (temp!=NULL){
-        // printf("node found %d\n",temp->val);
-        neighbour* insertLink = temp->link;
-        if (insertLink->val == key){
-            temp->link = insertLink->next;
-        }
-        neighbour* parent = insertLink;
-        neighbour* child = parent->next;
-        while(child!=NULL){
-            if (child->val == key){
-                parent->next = child->next;
-                break;
-            }parent = parent->next;
-            child = parent->next;
-        }
-        if (insertLink == NULL){
-            printf("neighbour %d not found\n",key);
-        }
-    }else{
-        printf("node not found\n");
-    }
-
-    return head;
-}
-
-int main(){
-    node* headNode;
-    headNode = insertNode(headNode,1);
-    headNode = insertNode(headNode,2);
-    headNode = insertNode(headNode,3);
-    headNode = insertNode(headNode,4);
-    headNode = insertNode(headNode,6);
-    headNode = insertNode(headNode,5);
+int main()
+{
+    node *headNode;
+    headNode = insertNode(headNode, 1);
+    headNode = insertNode(headNode, 2);
+    headNode = insertNode(headNode, 3);
+    headNode = insertNode(headNode, 4);
+    headNode = insertNode(headNode, 6);
+    headNode = insertNode(headNode, 5);
     show_node(headNode);
+    // show_rev(headNode);
 
     // insertion of node
-    headNode = insertNeighbour(headNode,1,3);
-    headNode = insertNeighbour(headNode,1,4);
-    headNode = insertNeighbour(headNode,2,1);
-    headNode = insertNeighbour(headNode,2,2);
-    headNode = insertNeighbour(headNode,2,3);
-    headNode = insertNeighbour(headNode,3,2);
-    headNode = insertNeighbour(headNode,3,5);
-    headNode = insertNeighbour(headNode,4,1);
-    headNode = insertNeighbour(headNode,5,3);
-    headNode = insertNeighbour(headNode,5,4);
-    headNode = insertNeighbour(headNode,5,5);
-    headNode = insertNeighbour(headNode,5,6);
+    headNode = insertNeighbour(headNode, 1, 3);
+    headNode = insertNeighbour(headNode, 1, 4);
+    headNode = insertNeighbour(headNode, 2, 1);
+    headNode = insertNeighbour(headNode, 2, 2);
+    headNode = insertNeighbour(headNode, 2, 3);
+    headNode = insertNeighbour(headNode, 3, 2);
+    headNode = insertNeighbour(headNode, 3, 5);
+    headNode = insertNeighbour(headNode, 4, 7);
+    headNode = insertNeighbour(headNode, 5, 3);
+    headNode = insertNeighbour(headNode, 5, 4);
+    headNode = insertNeighbour(headNode, 5, 5);
+    headNode = insertNeighbour(headNode, 5, 6);
     show_all(headNode);
+    
 
-    // deletion of node
-    headNode = deleteNeighbour(headNode,2,3);
-    headNode = deleteNeighbour(headNode,4,10);
-    headNode = deleteNeighbour(headNode,5,5);
-    show_all(headNode);
 }
-
-
-
