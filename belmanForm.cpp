e1988415f8572cefae8cc1c809d2b4f41a01d18c
Hrishabh Pandey <44145550+hrs2203@users.noreply.github.com>
2019-10-29 15:13:22 +0530

    Belman implimentation

---
+++ belmanForm.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void belmanFormAlgo(vector<pair<int,int> >adjList[],int start,int nodes){
+    int dist[nodes];
+    for (int i = 0; i < nodes; i++)
+        dist[i]=2e8;    //setting distance to inf.
+    dist[start-1]=0;  //setting start to 0
+
+    int edgeCount = 0;
+    int nbr,wt;
+
+    // for (int x = 0; x < nodes; x++){
+        for (int i = 0; i < nodes; i++){
+            edgeCount = adjList[i].size();
+            for (int j = 0; j < edgeCount; j++){
+                nbr = adjList[i].at(j).first;
+                wt  = adjList[i].at(j).second;
+                if (dist[nbr-1] > dist[i]+wt){
+                    dist[nbr-1] = dist[i]+wt;
+                }
+            }
+        }
+    // }
+    
+    for (int i = 0; i < nodes; i++)
+        cout << dist[i] << ' ';
+    cout << endl;
+}
+
+int main(){
+    int nodes,edges,a,b,w;
+    cin >> nodes >> edges;
+    vector< pair<int,int> > adjList[nodes];
+    for (int i = 0; i < edges; i++){
+        cin >> a >> b >> w;
+        adjList[a-1].push_back(make_pair(b,w));
+        adjList[b-1].push_back(make_pair(a,w));
+    }
+    int startPoint = 1;
+    belmanFormAlgo(adjList,startPoint,nodes);
+}
