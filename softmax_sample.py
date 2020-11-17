3eefb9f55aa3b87e5ac5824adf18e4e2cdb51f67
Hrishabh Pandey <44145550+hrs2203@users.noreply.github.com>
2020-11-18 00:02:25 +0530

    new changes

--- softmax_sample.py
+++ softmax_sample.py
@@ -20,6 +20,7 @@ def readFile(fileNameArg: str) -> list:
     fileContent = [item.split(",") for item in fileContentTemp.split("\n")]
     return fileContent[:-1]
 
+
 def parseCsv(inpArray: list):
     """Parse Input
 
@@ -34,7 +35,7 @@ def parseCsv(inpArray: list):
     for item in inpArray:
         X.append(item[:-1])
         Y.append(item[-1])
-    return np.array(X, dtype='float64'), np.array(Y, dtype='float64')
+    return np.array(X, dtype="float64"), np.array(Y, dtype="int")
 
 
 def sigmoid(x: float) -> float:
@@ -48,37 +49,101 @@ def sigmoid(x: float) -> float:
     """
     return 1 / (1 + np.exp(-1 * x))
 
-def hypothesis_softmax(x: list,q: list) -> list:
+
+def hypothesis_softmax(x: list, q: list) -> list:
     """hypothesis fxn for softmax
 
     Args:
-        x (list): X : training data
-        q (list): Q : theta
+        x (list): X : training data ( 1 X no of paramenters )
+        q (list): Q : theta ( output count X no of paramenters )
 
     Returns:
         list: Q.T * X
     """
-    tempRes = []
-    dataCount = x.shape[0]
-
-    for index in range(dataCount):
-        tempRes.append( np.dot(q[index], x[index].T) )
+    tempRes = np.exp(np.dot(q, x.T))
 
     tempRes /= np.sum(tempRes)
 
     return np.array(tempRes)
 
+def getIndex(hypothesis_output: list) -> int:
+    """get the class index
+
+    Args:
+        hypothesis_output (list): as said
+
+    Returns:
+        int: index of output class
+    """
+    max_index = 0
+    max_val = hypothesis_output[max_index]
+    for i in range(len(hypothesis_output)):
+        if ( hypothesis_output[i] >= max_val ):
+            max_val = hypothesis_output[i]
+            max_index = i
+    return max_index
+
+
+def cost_fxn(x: list, y: list, q: list) -> float:
+    """cost fxn for softmax logistic regression
+
+    Args:
+        x (list): training
+        y (list): training output
+        q (list): theta
+
+    Returns:
+        float: cost of hypothesis
+    """
+    totalCost = 0
+
+    itemCount = x.shape[0]
+    for index in range(1):
+        totalCost += np.log(hypothesis_softmax(x[index], q)[y[index][0]])
+
+    return -1 * totalCost
+
 
 if __name__ == "__main__":
     # print("DEFAULT")
+
     # read file
     x, y = parseCsv(readFile(TRAINING_FILE))
+
     # set variables
     sampleSize, parameterCount = x.shape
+    outputCount = len(set(y))
+    y = y.reshape((y.shape[0], 1))
+
     # set theta ( we use q )
-    q = np.random.rand(sampleSize, parameterCount)
+    q = np.random.rand(outputCount, parameterCount)
 
     # testing
-    y_temp = hypothesis_softmax(x,q)
-    print(y_temp.shape)
+    print("============= pre training =========== ")
+    # y_res = hypothesis_softmax(x[0], q)
+    completeLoss = cost_fxn(x, y, q)
+    print(f"cost: {completeLoss}")
+    # =============== accuracy ===============
+    correctCount = 0
+    for ind in range(sampleSize):
+        correctCount += int(getIndex( hypothesis_softmax( x[ind], q ) ) == y[ind][0])
+    print(f"accuracy: {(correctCount / sampleSize)* 100}")
+    # ========================================
 
+    # count = 80
+    # while count > 0:
+    #     count -= 1
+    #     #  ========= start training =============
+
+    #     #  =========  end training  =============
+
+    print("============ post training =========== ")
+    # y_res = hypothesis_softmax(x, q)
+    completeLoss = cost_fxn(x, y, q)
+    print(f"cost: {completeLoss}")
+    # =============== accuracy ===============
+    correctCount = 0
+    for ind in range(sampleSize):
+        correctCount += int(getIndex( hypothesis_softmax( x[ind], q ) ) == y[ind][0])
+    print(f"accuracy: {(correctCount / sampleSize)* 100}")
+    print("====================================== ")
