3413b1488830bdcfe3676081cc2bca1232149adb
Hrishabh Pandey <44145550+hrs2203@users.noreply.github.com>
2020-11-19 09:53:33 +0530

    Add files via upload
    sa

--- softmax_sample.py
+++ softmax_sample.py
@@ -1,5 +1,7 @@
 import numpy as np
 import csv
+import matplotlib.pyplot as plt
+
 
 TRAINING_FILE = "./training1.csv"
 TEST_FILE = "./test1.csv"
@@ -66,8 +68,9 @@ def hypothesis_softmax(x: list, q: list) -> list:
 
     return np.array(tempRes)
 
+
 def getIndex(hypothesis_output: list) -> int:
-    """get the class index
+    """get the output class index
 
     Args:
         hypothesis_output (list): as said
@@ -78,7 +81,7 @@ def getIndex(hypothesis_output: list) -> int:
     max_index = 0
     max_val = hypothesis_output[max_index]
     for i in range(len(hypothesis_output)):
-        if ( hypothesis_output[i] >= max_val ):
+        if hypothesis_output[i] >= max_val:
             max_val = hypothesis_output[i]
             max_index = i
     return max_index
@@ -104,6 +107,36 @@ def cost_fxn(x: list, y: list, q: list) -> float:
     return -1 * totalCost
 
 
+def indicator_fxn(y, k):
+    """ int(y===k) """
+    return int(y == k)
+
+
+def get_gradient(x, q, y):
+    correction = np.zeros(q.shape)
+    dataCount = x.shape[0]
+    parameterCount = x.shape[1]
+    outputCount = q.shape[0]
+
+    for index in range(dataCount):
+        x_temp = x[index]
+        new_hypo = hypothesis_softmax(x_temp, q)
+        temp_output = getIndex(new_hypo)
+        temp_hypo = np.zeros(new_hypo.shape)
+        for ind in range(outputCount):
+            if ind == temp_output:
+                temp_hypo[ind] = 1 - new_hypo[ind]
+            else:
+                temp_hypo[ind] = -new_hypo[ind]
+
+        for i in range(outputCount):
+            correction[i] += x_temp * temp_hypo[i]
+
+    correction /= dataCount
+
+    return correction
+
+
 if __name__ == "__main__":
     # print("DEFAULT")
 
@@ -113,6 +146,7 @@ if __name__ == "__main__":
     # set variables
     sampleSize, parameterCount = x.shape
     outputCount = len(set(y))
+    alpha = 0.001
     y = y.reshape((y.shape[0], 1))
 
     # set theta ( we use q )
@@ -121,29 +155,48 @@ if __name__ == "__main__":
     # testing
     print("============= pre training =========== ")
     # y_res = hypothesis_softmax(x[0], q)
+    # ============== calc cost ===============
     completeLoss = cost_fxn(x, y, q)
     print(f"cost: {completeLoss}")
     # =============== accuracy ===============
     correctCount = 0
     for ind in range(sampleSize):
-        correctCount += int(getIndex( hypothesis_softmax( x[ind], q ) ) == y[ind][0])
+        correctCount += int(getIndex(hypothesis_softmax(x[ind], q)) == y[ind][0])
     print(f"accuracy: {(correctCount / sampleSize)* 100}")
     # ========================================
 
-    # count = 80
-    # while count > 0:
-    #     count -= 1
-    #     #  ========= start training =============
+    # grad = get_gradient(x,q,y)
+    # print(grad.shape)
+    # print(q.shape)
 
-    #     #  =========  end training  =============
+    tempCost = []
+
+    count = 500
+    print("========== training start ============ ")
+    while count > 0:
+        count -= 1
+        # print(count, end=",")
+        #  ========= start training =============
+        temp_grad = get_gradient(x, q, y)
+        print(f"{count},{temp_grad.shape}")
+        q = q - (alpha * temp_grad)
+        tempCost.append(cost_fxn(x, y, q))
+
+        #  =========  end training  =============
+    print("\n============ training end ============ ")
 
     print("============ post training =========== ")
     # y_res = hypothesis_softmax(x, q)
+    # ============== calc cost ===============
     completeLoss = cost_fxn(x, y, q)
     print(f"cost: {completeLoss}")
     # =============== accuracy ===============
     correctCount = 0
     for ind in range(sampleSize):
-        correctCount += int(getIndex( hypothesis_softmax( x[ind], q ) ) == y[ind][0])
+        correctCount += int(getIndex(hypothesis_softmax(x[ind], q)) == y[ind][0])
     print(f"accuracy: {(correctCount / sampleSize)* 100}")
+    # ========================================
     print("====================================== ")
+    plt.plot(tempCost)
+    plt.show()
+
